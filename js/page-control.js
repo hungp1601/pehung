b0a5ddc32a11e35d495ae49b56045abcae0b7717
hungp1601 <59797651+hungp1601@users.noreply.github.com>
2021-03-20 10:23:06 -0700

    Update page-control.js

---
+++ js/page-control.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementById("menu-mobile").classList.toggle("none");
+    document.getElementById("title-1").classList.toggle("change-1");
+    document.getElementById("mySidenav").classList.toggle("side-nav");
+    document.getElementById("mySidenav").classList.toggle("none");
+    document.getElementById("menu-mobile").classList.toggle("tang-hinh");
+
+}
